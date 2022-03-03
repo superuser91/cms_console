ec66c25fc7de6b3180944f38206d0e1b86ff3250
dungnt2 <dungnt2.ctv@gmail.com>
2022-03-03 10:26:29 +0700

    update CmsConsoleServiceProvider.php

--- src/CmsConsoleServiceProvider.php
+++ src/CmsConsoleServiceProvider.php
@@ -6,9 +6,13 @@ use Illuminate\Support\ServiceProvider;
 
 class CmsConsoleServiceProvider extends ServiceProvider
 {
-    public function boot()
+    public function register()
     {
         $this->mergeConfigFrom(__DIR__ . '/../config/config.php', 'vgplay');
+    }
+
+    public function boot()
+    {
         $this->loadViewsFrom(__DIR__ . '/../resources/views', 'vgplay');
         $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
     }
